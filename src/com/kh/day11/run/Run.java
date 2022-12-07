9f5d3aa7651cf65f32496b461565573832a313f8
user1 <user1@DESKTOP-MPUKNTA>
2022-12-07 14:00:37 +0900

    ** Run파일 추가

---
+++ src/com/kh/day11/run/Run.java
@@ -0,0 +1,12 @@
+package com.kh.day11.run;
+
+import com.kh.day11.exercise.Exercise_StringTokenizer;
+import com.kh.day11.exercise.Exercise_Wrapper;
+
+public class Run {
+	public static void main(String[] args) {
+		Exercise_Wrapper wr = new Exercise_Wrapper();
+		Exercise_StringTokenizer exSt = new Exercise_StringTokenizer();
+		exSt.exercise1();
+	}
+}
